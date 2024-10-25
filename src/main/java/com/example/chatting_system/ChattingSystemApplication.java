7760d0c4919b1c2df146c9695dff9acc5e891ba3
vanguard-gpt <vanguard09@naver.com>
2024-10-25 21:49:59 +0900

    초기 설정

---
+++ src/main/java/com/example/chatting_system/ChattingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatting_system/ChattingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
