8747dd6d83d4fe3a3d9ad3d71a8b4a72a68de4f7
vanguard-gpt <vanguard09@naver.com>
2024-10-26 22:54:29 +0900

    이상 동작 문제

---
+++ src/main/java/handler/WebSocketChatHandler.java
@@ -0,0 +1,20 @@
+package handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws Exception {
+        String payload = textMessage.getPayload();
+        log.info("Received payload: {}", payload);
+
+        TextMessage welcomeMessage = new TextMessage("채팅에 참가하셨습니다.");
+        session.sendMessage(welcomeMessage);
+    }
+}
