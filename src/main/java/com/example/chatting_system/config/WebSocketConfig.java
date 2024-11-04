5c5b0fccb15c91d7425b609c7801cc553477dce7
vanguard-gpt <vanguard09@naver.com>
2024-11-04 23:56:18 +0900

    디렉토리 구조 변경
    ㅂㅅ같은 실수

--- src/main/java/com/example/chatting_system/config/WebSocketConfig.java
+++ src/main/java/com/example/chatting_system/config/WebSocketConfig.java
@@ -1,12 +1,13 @@
-package config;
+package com.example.chatting_system.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Configuration
+@Component
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final WebSocketHandler webSocketHandler;
--- src/main/java/com/example/chatting_system/handler/WebSocketChatHandler.java
+++ src/main/java/com/example/chatting_system/handler/WebSocketChatHandler.java
@@ -1,4 +1,4 @@
-package handler;
+package com.example.chatting_system.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
